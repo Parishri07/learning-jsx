f929f48f3ebbc9df2614f9d78a919b532b00b813
Parishri07 <136893851+Parishri07@users.noreply.github.com>
2023-08-12 22:11:53 +0530

    Update index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<h1>Hello</h1>,document.querySelector("#root"));
+//render function takes in two arguments as what to output and where to output it
