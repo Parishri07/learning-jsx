b6adb61bd9f8fbbe97c1818b933dce5c49de88f4
Parishri07 <136893851+Parishri07@users.noreply.github.com>
2023-08-13 15:44:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
-import React from "react";
+//JSX enables us to write HTML and JS code in JS file
+
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello</h1>,document.querySelector("#root"));
-//render function takes in two arguments as what to output and where to output it
+function App() {
+  const [name, setName] = useState("Neil"); // Default name
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  var currentDate = new Date();
+  var currentYear = currentDate.getFullYear();
+  var myName = "Parishri";
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={name}
+        onChange={handleNameChange}
+      />
+      <h1>Hello {name}!</h1>
+
+      <p>Created by {myName}</p>
+      <p>Copyright {currentYear}</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
+
+//render means providing, render function takes in two arguments as what to output and where to output it
+//for it to be able to identify that it is JS expression write in "{//js code}"
